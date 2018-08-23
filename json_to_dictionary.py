5dc2420feb8855dd51074b052ffd1e07274cf4f5
Lawrence Nara <njielitumbe@gmail.com>
2018-08-23 09:59:06 -0400

    Complete more excercises

---
+++ json_to_dictionary.py
@@ -0,0 +1,13 @@
+import json
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+with open('company.json', 'w') as file:
+
+    file.write(json.dumps(d))
+
+
--- multilevel_dictionary.py
+++ multilevel_dictionary.py
@@ -6,3 +6,17 @@ d = {"employees":[{"firstName": "John", "lastName": "Doe"},
 
 
 print(d['employees'][1]['lastName'])
+
+
+# add to multilevel dictionary
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+print("before add", d['employees'])
+d['employees'].append({'firstName': 'Albert', 'lastName': 'Bert'})
+
+print("after add", d['employees'])
+
